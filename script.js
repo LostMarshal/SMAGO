f7b78fd7eeb45c9abe04d0d4b9d849e7c56ca357
LostMarshal <fardilabintang476@gmail.com>
2024-05-27 01:39:15 +0700

    add page soal-test & scoring system

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+var points = 0;
+        var answers = [];
+        var totalQuestions = 3; // Update this with the total number of questions
+        
+        function trueAns(q) {
+          if(answers[q] != 1) {
+            points += 10; //Only + will not change the score because It's adding without putting. But here we have both adding && putting. 
+            answers[q] = 1;
+          }
+        }
+        
+        function falseAns(q) {
+          if(answers[q] == 1){
+            points -= 10;
+            answers[q] = -1;
+          }
+        }
+        
+        function totalSum() {
+          if (answers.length < totalQuestions) {
+            alert("Mohon jawab semua soal sebelum melihat skor!");
+          } else {
+            document.querySelector("#sc").innerHTML = points;
+          }
+        }
